820dc83db23c2f3a9f203ffd17f9e8ae916518a1
Adrisa <adrisansyarif@gmail.com>
2025-08-12 23:31:14 +0700

    feat: add initial  landing page

---
+++ src/components/organisms/Footer/index.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="h-1/12 flex justify-center items-center bg-green-800 text-white">
+      Copyright of UNDP and Bappenas, 2025
+    </footer>
+  );
+};
+export default Footer;
