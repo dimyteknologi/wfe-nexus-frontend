5c74389d45fe65723bd95c63a7ce390e2fc3a126
Adrisa <adrisansyarif@gmail.com>
2025-08-16 00:31:54 +0700

    feat/add header dss interface, scenario menu, and placeholder UI for graph

--- src/components/grid/DSSGrid.layout.tsx
+++ src/components/grid/DSSGrid.layout.tsx
@@ -4,10 +4,8 @@ interface GridLayoutProps {
   children: React.ReactNode;
 }
 
-export function DSSGridLayout({ children }: GridLayoutProps) {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-8">
-      {children}
-    </div>
-  );
-}
+const DSSGridLayout = ({ children }: GridLayoutProps) => {
+  return <div className="grid grid-cols-1 gap-4 pb-8">{children}</div>;
+};
+
+export default DSSGridLayout;
