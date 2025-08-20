62706298105c6ebb5897ed527a9c7a27e3bb290e
xnuxuz <adam.albani.t@gmail.com>
2025-08-20 12:18:56 +0700

    fix: hardcode baseline

--- src/app/dss-interface/page.tsx
+++ src/app/dss-interface/page.tsx
@@ -59,6 +59,35 @@ const dummyData: iTableData[] = Array.from({ length: 42 }).map((_, i) => ({
   baseline_2: Math.random() + i,
 }));
 
+const baselineMetrics = {
+  gdrpInBillions: [
+    99641.32, 106174.68, 111424.08, 116673.49, 126748.69, 132453.57, 141125.54,
+    148358.45, 157317.84, 163946.85, 157710.59, 166941.49, 177470.89, 187051.65,
+    196972.85, 207490.83, 218644.43, 230475.43, 243028.81, 256353.06, 270500.47,
+    285527.49, 301495.12, 318469.35, 336521.63, 355729.39, 376176.63, 397954.59,
+    421162.45, 445908.14, 472309.25, 500494.02, 530602.44, 562787.5, 597216.6,
+    634073.05,
+  ],
+  economicGrowth: [
+    6.0, 6.56, 4.94, 4.71, 8.64, 4.5, 6.55, 5.13, 6.04, 4.21, -3.8, 5.85, 6.31,
+    5.4, 5.3, 5.34, 5.38, 5.41, 5.45, 5.48, 5.52, 5.56, 5.59, 5.63, 5.67, 5.71,
+    5.75, 5.79, 5.83, 5.88, 5.92, 5.97, 6.02, 6.07, 6.12, 6.17,
+  ],
+  projectedPopulation: [
+    2172289, 2199394, 2225383, 2250120, 2273579, 2295778, 2316489, 2336009,
+    2353915, 2370488, 2370488, 2370488, 2370488, 2580557, 2615567, 2651053,
+    2687020, 2723474, 2760424, 2797875, 2835834, 2874308, 2913303, 2952828,
+    2992890, 3033494, 3074650, 3116364, 3158644, 3201497, 3244932, 3288956,
+    3333578, 3378804, 3424645, 3471107,
+  ],
+  gdrpPerCapita: [
+    45.87, 48.27, 50.07, 51.85, 55.75, 57.69, 60.92, 63.51, 66.83, 69.16, 66.53,
+    70.42, 74.87, 72.48, 75.31, 78.27, 81.37, 84.63, 88.04, 91.62, 95.39, 99.34,
+    103.49, 107.85, 112.44, 117.27, 122.35, 127.7, 133.34, 139.28, 145.55,
+    152.17, 159.17, 166.56, 174.39, 182.67,
+  ],
+};
+
 const DSSPage = () => {
   useInitializeData();
   const dispatch = useAppDispatch();
@@ -130,7 +159,6 @@ const DSSPage = () => {
     // );
 
     const baselineName = "Baseline (Historical Projection)";
-
     const scenarioA_Recipe = savedScenarios.find(
       (s: SimulationState) => s.simulationName === simulationState.scenario_a,
     );
@@ -198,13 +226,13 @@ const DSSPage = () => {
     const metricsA = scenarioA_ProjectionData
       ? getMetricsFromProjection(
           scenarioA_ProjectionData,
-          scenarioA_Recipe || historicalGdpData,
+          scenarioA_Recipe || baselineMetrics,
         )
       : null;
     const metricsB = scenarioB_ProjectionData
       ? getMetricsFromProjection(
           scenarioB_ProjectionData,
-          scenarioB_Recipe || historicalGdpData,
+          scenarioB_Recipe || baselineMetrics,
         )
       : null;
 
@@ -215,7 +243,7 @@ const DSSPage = () => {
         title: "GDRP [Bilion Rp/year]",
         type: "bar",
         series: [
-          { name: projectionData.tabel, data: activeMetrics.gdrpInBillions },
+          { name: projectionData.tabel, data: baselineMetrics.gdrpInBillions },
           ...(metricsA
             ? [
                 {
@@ -239,7 +267,7 @@ const DSSPage = () => {
         title: "Economic Growth (%/year)",
         type: "line",
         series: [
-          { name: projectionData.tabel, data: activeMetrics.economicGrowth },
+          { name: projectionData.tabel, data: baselineMetrics.economicGrowth },
           ...(metricsA
             ? [
                 {
@@ -265,7 +293,7 @@ const DSSPage = () => {
         series: [
           {
             name: projectionData.tabel,
-            data: activeMetrics.projectedPopulation,
+            data: baselineMetrics.projectedPopulation,
           },
           ...(metricsA
             ? [
@@ -290,7 +318,7 @@ const DSSPage = () => {
         title: "GDRP Per capita [Milion Rp/cap/year]",
         type: "bar",
         series: [
-          { name: projectionData.tabel, data: activeMetrics.gdrpPerCapita },
+          { name: projectionData.tabel, data: baselineMetrics.gdrpPerCapita },
           ...(metricsA
             ? [
                 {
